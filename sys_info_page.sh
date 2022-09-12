01f73992fdcd63b09a057711cec5496c7f173bf7
Paul Adunola <paul.adunola@ufl.edu>
2022-09-12 10:04:06 -0400

    Adding text to sys

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -2,9 +2,9 @@
 # Program to output a system information page
 echo "<html>
   <head>
-      <title>Page Title</title>
+      <title>System Information Report</title>
   </head>
   <body>
-      Page body.
+      System Information Report
   </body>
 </html>"
